d5d723f3eaf89e40bc3a1f1db2a582e91ae96b36
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-10-03 14:04:26 +0200

    added a entity class for testing

---
+++ app/src/test/java/Task.kt
@@ -0,0 +1,10 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// test to see if i can follow the slides
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val title: String,
+    val description: String
+)
