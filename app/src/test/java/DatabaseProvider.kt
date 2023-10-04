64a2b94dfe40ebf5e9907deaabdb9562dcd692c4
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-10-04 12:48:36 +0200

    followed the slides but i have no idea how to organise stuff

---
+++ app/src/test/java/DatabaseProvider.kt
@@ -0,0 +1,20 @@
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+    private var instance: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        return instance ?: synchronized(this) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+    }
+
+    private fun buildDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "my-database"
+        ).build()
+    }
+}
