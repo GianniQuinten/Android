7231fffae2f9659ab33f2df7713aaca1855ea3eb
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-10-12 14:02:54 +0200

    Update Register.kt

--- app/src/main/java/com/example/hirehubresources/Register.kt
+++ app/src/main/java/com/example/hirehubresources/Register.kt
@@ -1,31 +1,74 @@
 package com.example.hirehubresources
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.hirehubresources.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-class Register : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
-        setContentView(R.layout.activity_register)
+        setContentView(view)
 
-        // references the strings in the layout xml
-        binding.email.hint = getString(R.string.email)
-        binding.password.hint = getString(R.string.password)
+        auth = FirebaseAuth.getInstance()
 
-        binding.registerBtn.setOnClickListener {
+        // binding setter
+        val editTextEmail = binding.email
+        val editTextPassword = binding.password
+        val buttonReg = binding.registerBtn
+
+        // register button functionality
+        buttonReg.setOnClickListener {
             // Get user input
-            val userEmail = binding.email.text.toString()
-            val userPassword = binding.password.text.toString()
+            val userEmail = editTextEmail.text.toString()
+            val userPassword = editTextPassword.text.toString()
 
-            // If registration is successful,go to the register activity
-            val intent = Intent(this, Register::class.java)
-            startActivity(intent)
+            // Registration empty handler
+            if (TextUtils.isEmpty(userEmail)) {
+                Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(userPassword)) {
+                Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Create a user using Firebase Authentication
+            auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener() { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(
+                            baseContext,
+                            "You created your account.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        updateUI(null)
+                    }
+                }
         }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        //  UI updates here if registry is successfull
     }
 }
